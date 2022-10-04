f07dd0c54d981a4fdb75e089ebf9eedde8d71d0b
Jake Hughey <jakejhughey@gmail.com>
2022-10-04 16:02:16 -0500

    updated google apps script.

--- code/webhook.js
+++ code/webhook.js
@@ -3,32 +3,51 @@
 // https://stackoverflow.com/questions/66154780/send-a-post-request-using-google-apps-script
 // https://stackoverflow.com/questions/58359417/you-do-not-have-permission-to-call-urlfetchapp-fetch
 
-function atEdit(e) {
-  var sheets = ['groups', 'show_columns', 'sorting', 'viewers', 'github', 'data'];
+function dispatch(repo_url) {
+  var url = repo_url.replace('github\.com', 'api.github.com/repos') + '/dispatches';
+  var pat_file = DriveApp.getFileById('1xOU3teX79HsTTljvdjfn9vUH4__iVoyy');
+  var auth = 'Bearer ' + pat_file.getBlob().getDataAsString();
+
+  var headers = {
+    'Accept': 'application/vnd.github+json',
+    'Authorization': auth
+  };
+  var data = {
+    'event_type': 'webhook'
+  };
+  var options = {
+    'method': 'post',
+    'contentType': 'application/json',
+    'headers': headers,
+    'payload': JSON.stringify(data)
+  };
+  UrlFetchApp.fetch(url, options);
+}
+
+function at_edit(e) {
+  var sheets = ['groups', 'show_columns', 'sorting', 'viewers', 'github'];
   var sheet_now = e.range.getSheet().getName();
-  var enabled = e.source.getSheetByName('github').getRange('B2').getValue();
+  var gh = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('github');
+  var enabled = gh.getRange('B2').getValue();
 
   if (enabled == 1 && sheets.includes(sheet_now)) {
-    var repo_url = e.source.getSheetByName('github').getRange('A2').getValue();
-    var url = repo_url.replace('github\.com', 'api.github.com/repos') + '/dispatches';
+    var repo_url = gh.getRange('A2').getValue();
+    dispatch(repo_url);
+  }
+}
 
-    var pat_file = DriveApp.getFileById('1xOU3teX79HsTTljvdjfn9vUH4__iVoyy');
-    var pat = pat_file.getBlob().getDataAsString();
-    var auth = 'Bearer ' + pat;
+function at_change(e) {
+  user_now = e.user.getEmail();
+  var gh = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('github');
+  var enabled = gh.getRange('B2').getValue();
 
-    var headers = {
-      'Accept': 'application/vnd.github+json',
-      'Authorization': auth
-    };
-    var data = {
-      'event_type': 'webhook'
-    };
-    var options = {
-      'method': 'post',
-      'contentType': 'application/json',
-      'headers': headers,
-      'payload': JSON.stringify(data)
-    };
-    UrlFetchApp.fetch(url, options);
+  Logger.log(e.authMode);
+  Logger.log(e.changeType);
+  Logger.log(user_now);
+
+  // should correspond to changes made by surveycto and not via github actions
+  if (enabled == 1 && user_now == '' && e.changeType == 'INSERT_ROW') {
+    var repo_url = gh.getRange('A2').getValue();
+    dispatch(repo_url);
   }
 }
